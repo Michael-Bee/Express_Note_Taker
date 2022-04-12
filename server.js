e6b2014f5f32ef0df28952a6450a9c763a6719ea
Michael Bee <bee.michael.02@gmail.com>
2022-04-11 21:02:58 -0500

    Get and Post routes done, need Delete route.

--- server.js
+++ server.js
@@ -2,26 +2,32 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
-
 const PORT = process.env.port || 3001;
-
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-
 app.get('/notes', (req,res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
 
 ////Routes: 
 // (GET to see all notes) 
 app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            res.status(201).json(parsedData);
+    }
+  // Log our request to the terminal
+    console.info(`${req.method} request received to get notes`);
+    });
 });
+
 
 // (POST to add note) 
 app.post('/api/notes', (req, res) => {
@@ -41,8 +47,8 @@ app.post('/api/notes', (req, res) => {
                 console.error(err);
             } else {
                 const parsedData = JSON.parse(data);
-                parsedData.push(newNote);
                 
+                parsedData.push(newNote);
                 fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
                     err ? console.error(err) : console.info(`\nData written to ${destination}`)
                 );
