805deb8b91cc996343c9bdffe2303f00d296a96d
Michael Bee <bee.michael.02@gmail.com>
2022-04-11 20:57:10 -0500

    Finished POST route.

--- server.js
+++ server.js
@@ -36,8 +36,24 @@ app.post('/api/notes', (req, res) => {
             id: uuid(),
         };
 
-    readAndAppend(newNote, './db/db.json');
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                parsedData.push(newNote);
+                
+                fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
+                    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+                );
+            }
+        });
 
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        console.log(response);
         res.json(`Note added successfully 🚀`);
     } else {
         res.error('Error in adding note');
