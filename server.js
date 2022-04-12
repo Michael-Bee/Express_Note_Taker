f260f9d888702551282dd7eb2dbf69e2b839865c
Michael Bee <bee.michael.02@gmail.com>
2022-04-11 20:21:09 -0500

    Required in UUID helper, made get for homepage index.html.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.port || 3001;
 
@@ -26,6 +27,13 @@ app.get('/notes', (req,res) =>
 
 // (app.DELETE to remove notes)
 
+
+
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
