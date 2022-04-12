8e41f27caee051dd76ec652fb2d7a830a5a24808
Michael Bee <bee.michael.02@gmail.com>
2022-04-11 20:15:38 -0500

    Finished setting up server, started pseudocode.

--- server.js
+++ server.js
@@ -14,3 +14,15 @@ app.use(express.static('public'));
 app.get('/notes', (req,res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+//Routes: 
+// (app.GET request to see notes) 
+// (app.POST request to add note) 
+// //(fs.READ request to see notes) 
+// //(fs.WRITE to update notes)
+// (app.DELETE to remove notes)
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
