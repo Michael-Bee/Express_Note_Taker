8b7d035c1bfdb18910ff0b37851d72b7cb835e29
Michael Bee <bee.michael.02@gmail.com>
2022-04-12 08:21:31 -0500

    Finished delete route, ready to deploy.

--- server.js
+++ server.js
@@ -8,30 +8,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.get('/notes', (req,res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
+    res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 ////Routes: 
 // (GET to see all notes) 
 app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for notes`);
+    console.info(`${req.method} request received to GET notes`);
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
         } else {
             const parsedData = JSON.parse(data);
             res.status(201).json(parsedData);
-    }
-  // Log our request to the terminal
-    console.info(`${req.method} request received to get notes`);
+        }
     });
 });
 
-
 // (POST to add note) 
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
+    console.info(`${req.method} request received to POST a note`);
 
     const { title, text } = req.body;
 
@@ -68,10 +63,22 @@ app.post('/api/notes', (req, res) => {
 
 // (DELETE to remove notes)
 app.delete('/user', (req, res) => {
-    res.send('Got a DELETE request at /user')
+    console.info(`${req.method} request received to DELETE a note`);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+
+        const id = parsedData.findIndex(({id}) => id === req.params.id);
+        if (id >= 0) {
+            parsedNotes.splice(id, 1);
+        }
+        fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
+            err ? console.error(err) : console.info(`Note deleted successfully 🚀`))
+        }
+    });
 })
-
-
 
 //// GET Route for homepage
 app.get('*', (req, res) =>
