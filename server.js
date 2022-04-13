9d0b066ee4afcc2c8cf329a2636675261a232cf8
Michael Bee <bee.michael.02@gmail.com>
2022-04-13 15:40:25 -0500

    Fix for port issues.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const app = express();
